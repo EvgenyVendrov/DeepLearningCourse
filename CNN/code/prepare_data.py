fa578f99b013831b2cbcb618b5743e0da3dc3bb8
Evgeny Vendrov <evgenyv94@gmail.com>
2020-01-13 12:56:43 +0200

    prepare data changed for the better

--- CNN/code/prepare_data.py
+++ CNN/code/prepare_data.py
@@ -8,12 +8,46 @@ import glob
 import os
 import sys
 
-path = sys.argv[1]
-folder = sys.argv[2] + r'\*.' + sys.argv[3]
+postfix = sys.argv[5]
+path_for_dog_train = sys.argv[1] + r'\*.' + postfix
+path_for_cat_train = sys.argv[2] + r'\*.' + postfix
+path_for_dog_test = sys.argv[3] + r'\*.' + postfix
+path_for_cat_test = sys.argv[4] + r'\*.' + postfix
+folder_for_train = sys.argv[6]
+folder_for_test = sys.argv[7]
+
 num = 0
-for filename in glob.glob(folder):
+for filename in glob.glob(path_for_dog_train):
     im = Image.open(filename)
     im = im.resize((200, 200), Image.ANTIALIAS)
-    n_path = os.path.join(path, str(num)) + '.' + sys.argv[3]
-    im.save(n_path)
+    new_path = os.path.join(folder_for_train, "dog" + str(num)) + '.' + postfix
+    im.save(new_path)
     num += 1
+print("finished ", num, " images of dog_train")
+
+num = 0
+for filename in glob.glob(path_for_cat_train):
+    im = Image.open(filename)
+    im = im.resize((200, 200), Image.ANTIALIAS)
+    new_path = os.path.join(folder_for_train, "cat" + str(num)) + '.' + postfix
+    im.save(new_path)
+    num += 1
+print("finished ", num, " images of cat_train")
+
+num = 0
+for filename in glob.glob(path_for_dog_test):
+    im = Image.open(filename)
+    im = im.resize((200, 200), Image.ANTIALIAS)
+    new_path = os.path.join(folder_for_test, "dog" + str(num)) + '.' + postfix
+    im.save(new_path)
+    num += 1
+print("finished ", num, " images of dog_test")
+
+num = 0
+for filename in glob.glob(path_for_cat_test):
+    im = Image.open(filename)
+    im = im.resize((200, 200), Image.ANTIALIAS)
+    new_path = os.path.join(folder_for_test, "cat" + str(num)) + '.' + postfix
+    im.save(new_path)
+    num += 1
+print("finished ", num, " images of cat_test")
